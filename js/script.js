be098cc4af7c001b922e0d3a790db8ef4e820aa2
Vitra Chandra <vitrachandra@gmail.com>
2018-05-06 21:41:17 +0700

    fix type filter

--- js/script.js
+++ js/script.js
@@ -23,16 +23,15 @@ function openNav() {
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
 	document.getElementById("main").style.marginRight= "0";
-	$(".sidenav").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
-    function(e) {
-    	$(".legend").each( function() {
-				t = $(this).attr('title');
-				$(this).text(truncateWithEllipses(t, 15));
-			});
-			t2 = $("#pulauName").attr('title');
-			$("#pulauName").text(t2);
-			$("#pulauName").removeAttr('title');
-    })
+	$(".sidenav").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+		$(".legend").each( function() {
+			t = $(this).attr('title');
+			$(this).text(truncateWithEllipses(t, 15));
+		});
+		t2 = $("#pulauName").attr('title');
+		$("#pulauName").text(t2);
+		$("#pulauName").removeAttr('title');
+	})
 }
 
 function readDataFile() {
@@ -46,22 +45,22 @@ function truncateWithEllipses(text, max) {
 
 //NUMBER FORMAT
 numeral.register('locale', 'id', {
-    delimiters: {
-        thousands: '.',
-        decimal: ','
-    },
-    abbreviations: {
-        thousand: 'ribu',
-        million: 'juta',
-        billion: 'miliar',
-        trillion: 'triliun'
-    },
-    ordinal : function (number) {
-        return number === 1 ? 'er' : 'ème';
-    },
-    currency: {
-        symbol: 'Rp '
-    }
+		delimiters: {
+				thousands: '.',
+				decimal: ','
+		},
+		abbreviations: {
+				thousand: 'ribu',
+				million: 'juta',
+				billion: 'miliar',
+				trillion: 'triliun'
+		},
+		ordinal : function (number) {
+				return number === 1 ? 'er' : 'ème';
+		},
+		currency: {
+				symbol: 'Rp '
+		}
 });
 numeral.locale('id');
 
@@ -104,6 +103,7 @@ function readData(yFrom, yTo, iName, dType) {
 	numRegionMapArray = [];
 	numRegionMapArrayDirty = [];
 	lossRegionMapArray = [];
+	lossRegionMapArrayDirty = [];
 	numRegionMapArray.push(regionMapLabel);
 	lossRegionMapArray.push(regionMapLabel);
 	barChartArray = [];
@@ -161,7 +161,7 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 								
-								case '2':
+								case 2:
 									totalDisasterCount = numFlood;
 									totalLossCount = lossFlood;
 
@@ -181,7 +181,7 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 
-								case '3':
+								case 3:
 									totalDisasterCount = numQuake;
 									totalLossCount = lossQuake;
 
@@ -200,7 +200,7 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 
-								case '4':
+								case 4:
 									totalDisasterCount = numFFire;
 									totalLossCount = lossFFire;
 
@@ -219,9 +219,9 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 
-								case '5':
+								case 5:
 									totalDisasterCount = numFlood + numQuake;
-									totalLossCount = lossFlodd + lossQuake;
+									totalLossCount = lossFlood + lossQuake;
 
 									numRegionMapData = [province, totalDisasterCount];
 
@@ -239,9 +239,9 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 
-								case '6':
+								case 6:
 									totalDisasterCount = numFlood + numFFire;
-									totalLossCount = lossFlodd + lossFFire;
+									totalLossCount = lossFlood + lossFFire;
 
 									numRegionMapData = [province, totalDisasterCount];
 
@@ -259,7 +259,7 @@ function readData(yFrom, yTo, iName, dType) {
 									sumLossCount += totalLossCount;
 									break;
 
-								case '7':
+								case 7:
 									totalDisasterCount = numQuake + numFFire;
 									totalLossCount = lossQuake + lossFFire;
 
@@ -280,7 +280,7 @@ function readData(yFrom, yTo, iName, dType) {
 									break;
 							}
 							numRegionMapArrayDirty.push(numRegionMapData);
-							lossRegionMapArray.push(lossRegionMapData);
+							lossRegionMapArrayDirty.push(lossRegionMapData);
 							if (iName == pulau) {
 								barChartArrayDirty.push(barChartData);
 							}
@@ -296,42 +296,106 @@ function readData(yFrom, yTo, iName, dType) {
 		arrNumQuake = ["Gempa Bumi", totalNumQuake];
 		arrNumFFire = ["Kebakaran Hutan", totalNumFFire];
 		pieChartArray.push(arrNumFlood, arrNumQuake, arrNumFFire);
-		console.log(barChartArrayDirty)
-		console.log(yTo-yFrom+1)
-		for (i = 0; i < barChartArrayDirty.length/(yTo-yFrom+1); i++) {
+		for (i = 0; i < barChartArrayDirty.length/(yTo - yFrom+1); i++) {
 			pulau = barChartArrayDirty[i][0];
 			totalLossFire = barChartArrayDirty[i][3];
 			totalLossQuake = barChartArrayDirty[i][2];
 			totalLossFlood = barChartArrayDirty[i][1];
 			for (j = 0; j < barChartArrayDirty.length; j++) {
-		    	if (i != j) {
-		    		if (barChartArrayDirty[i][0] == barChartArrayDirty[j][0]) {
-		    			totalLossFire += barChartArrayDirty[j][3];
+					if (i != j) {
+						if (barChartArrayDirty[i][0] == barChartArrayDirty[j][0]) {
+							totalLossFire += barChartArrayDirty[j][3];
 							totalLossQuake += barChartArrayDirty[j][2];
 							totalLossFlood += barChartArrayDirty[j][1];
-		    		}
-		    	}
+						}
+					}
 			}
 			barChartArray.push([pulau, totalLossFlood, totalLossQuake, totalLossFire]);
 		}
-		for (i = 0; i < numRegionMapArrayDirty.length/(yTo-yFrom+1); i++) {
+		for (i = 0; i < numRegionMapArrayDirty.length / (yTo - yFrom + 1); i++) {
 			pulau = numRegionMapArrayDirty[i][0];
 			totalDisasterDirty = 0
 			for (j = 0; j < numRegionMapArrayDirty.length; j++) {
-		    	if (i != j) {
-		    		if (numRegionMapArrayDirty[i][0] == numRegionMapArrayDirty[j][0]) {
-		    			totalDisasterDirty += numRegionMapArrayDirty[j][1];
-		    		}
-		    	}
+					if (i != j) {
+						if (numRegionMapArrayDirty[i][0] == numRegionMapArrayDirty[j][0]) {
+							totalDisasterDirty += numRegionMapArrayDirty[j][1];
+						}
+					}
 			}
 			numRegionMapArray.push([pulau, totalDisasterDirty]);
 		}
-		console.log(numRegionMapArray) 
-		google.charts.setOnLoadCallback(function() {
-			drawRegionMap(numRegionMapArray, black);
-			drawBarChart(barChartArray);
-			drawPieChart(pieChartArray, blue, brown, yellow); 
-		});
+		for (i = 0; i < lossRegionMapArrayDirty.length / (yTo - yFrom + 1); i++) {
+			pulau = lossRegionMapArrayDirty[i][0];
+			totalLossDirty = 0
+			for (j = 0; j < lossRegionMapArrayDirty.length; j++) {
+					if (i != j) {
+						if (lossRegionMapArrayDirty[i][0] == lossRegionMapArrayDirty[j][0]) {
+							totalLossDirty += lossRegionMapArrayDirty[j][1];
+						}
+					}
+			}
+			lossRegionMapArray.push([pulau, totalLossDirty]);
+		}
+
+		switch (dType) {
+			case 1 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, black, 'map');
+					drawRegionMap(lossRegionMapArray, black, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, blue, brown, yellow); 
+				});
+				break;
+			case 2 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, blue, 'map');
+					drawRegionMap(lossRegionMapArray, blue, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, blue, grey, grey); 
+				});
+				break;
+			case 3 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, brown, 'map');
+					drawRegionMap(lossRegionMapArray, brown, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, grey, brown, grey); 
+				});
+				break;
+			case 4 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, yellow, 'map');
+					drawRegionMap(lossRegionMapArray, yellow, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, grey, grey, yellow); 
+				});
+				break;
+			case 5 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, black, 'map');
+					drawRegionMap(lossRegionMapArray, black, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, blue, brown, grey); 
+				});
+				break;
+			case 6 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, black, 'map');
+					drawRegionMap(lossRegionMapArray, black, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, blue, grey, yellow); 
+				});
+				break;
+			case 7 :
+				google.charts.setOnLoadCallback(function() {
+					drawRegionMap(numRegionMapArray, black, 'map');
+					drawRegionMap(lossRegionMapArray, black, 'map1');
+					drawBarChart(barChartArray);
+					drawPieChart(pieChartArray, grey, brown, yellow); 
+				});
+				break;
+			
+		}
 		$("#jumlah_bencana").text(numeral(sumDisasterCount).format('0,00') + ' kejadian');
 		$("#kerugian").text(numeral(sumLossCount).format('$0.00 a'));
 	})
@@ -358,7 +422,7 @@ function drawPlotMap() {
 	chart.draw(x, options);
 };
 
-function drawRegionMap(array, color) {
+function drawRegionMap(array, color, id) {
 	var mapArray = google.visualization.arrayToDataTable(array);
 
 	var options = {
@@ -367,9 +431,8 @@ function drawRegionMap(array, color) {
 		datalessRegionColor: '#ffffff',
 		colorAxis: {colors: [ '#f8f8f8', color ]}
 	};
-	var chart = new google.visualization.GeoChart(document.getElementById('map'));
+	var chart = new google.visualization.GeoChart(document.getElementById(id));
 	chart.draw(mapArray, options);
-	console.log('im very wrong')
 };
 
 function drawBarChart(array) {
@@ -429,27 +492,21 @@ $("#mySidenav").change(function () {
 	if (iName == "Bali dan Nusa Tenggara") {
 		iName = "Nusa Tenggara";
 	}
-	isFlood = false;
-	isQuake = false;
-	isFFire = false;
-	$("#disasterType:checkbox:checked").each(function() {
-		isFlood = ( $(this).text() == 'Banjir' || isFlood );
-		isQuake = ( $(this).text() == 'Gempa Bumi' || isQuake );
-		isFFire = ( $(this).text() == 'Kebakaran Hutan' || isFFire );
-	})
+	isFlood = ( $('#banjir').attr('name') == 'banjir' && $('#banjir').is(':checked'));
+	isQuake = ( $('#gempabumi').attr('name') == 'gempabumi' && $('#gempabumi').is(':checked'));
+	isFFire = ( $('#kebakaran').attr('name') == 'kebakaran' && $('#kebakaran').is(':checked'));
 
 	dType = ( (isFlood && isQuake && isFFire) ? 1 : 
 						( (isFlood && isQuake) ? 5 : 
 							( (isFlood && isFFire) ? 6 : 
 								( (isQuake && isFFire) ? 7 : 
-									( isFlood ? 1 : 
-										( isQuake ? 2 :  
-											( isFFire ? 3 : 1 ) ) ) ) ) ) );
-
+									( isFlood ? 2 : 
+										( isQuake ? 3 :  
+											( isFFire ? 4 : 1 ) ) ) ) ) ) );
 	readData(yFrom, yTo, iName, dType);
 
 	$("#selectPulau option:selected" ).each(function() {
-  	var pulau = $(this).text();
+		var pulau = $(this).text();
 		$("#pulauName").text(truncateWithEllipses(pulau, 14));
 		$("#pulauName").attr('title', pulau);
 	});
